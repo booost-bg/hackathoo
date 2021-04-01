db099b08a1badea6b081e4dad2fb008b19c79380
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-04-01 11:43:53 +0300

    feat: implement basic timer

--- src/scenes/Timer.js
+++ src/scenes/Timer.js
@@ -1,4 +1,5 @@
 import Scene from "./Scene";
+import { pad } from "../core/utils";
 
 export default class Timer extends Scene {
   constructor() {
@@ -6,7 +7,37 @@ export default class Timer extends Scene {
   }
 
   async onCreated() {
+    this.createCountdownTimer();
+  }
+
+  getDates() {
     const settings = JSON.parse(localStorage.getItem("hackathonSettings"));
-    console.log(settings);
+    const startDate = this.parseHtmlDateTime(settings.startTime);
+    const endDate = this.parseHtmlDateTime(settings.endTime);
+    return { startDate, endDate };
+  }
+
+  parseHtmlDateTime(dateTime) {
+    const dateArr = dateTime.replace(/T|:/g, "-").split("-");
+    dateArr[1] = pad(dateArr[1] - 1);
+    const date = new Date(...dateArr);
+    return date;
+  }
+
+  createCountdownTimer() {
+    const { startDate, endDate } = this.getDates();
+    let startDateTime = startDate.getTime();
+    const endDateTime = endDate.getTime();
+    const timer = setInterval(() => {
+      let distance = endDateTime - startDateTime;
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      startDateTime += 1000;
+      console.log(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
+    }, 1000);
   }
 }
