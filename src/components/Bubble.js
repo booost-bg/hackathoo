f68d0a5d0cea63af35fc6c7f1eadb35ed8583f56
radotooo <radotooo@gmail.com>
2021-04-02 18:27:34 +0300

    refactor: code improvements

--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -1,55 +1,90 @@
-import { Container, Graphics, Sprite, Point } from 'pixi.js';
+import { Graphics } from 'pixi.js';
 import gsap from 'gsap';
 import { random } from '../core/utils';
 
-export default class Bubble extends Container {
-  constructor() {
-    super();
+const EVENTS = {
+  ANIMATION_DONE: 'animation_done',
+};
 
-    this._init();
+/**
+ * Initializes a new instance of Bubble
+ * @class
+ * @extends {PIXI.Graphics}
+ */
+export default class Bubble extends Graphics {
+  /**
+   * @param {Number} x The X coordinate of the center of the circle
+   * @param {Number} x The Y coordinate of the center of the circle
+   * @param {Number} radius The radius of the circle
+   * @param {Number} color The color of the circle
+   */
+  constructor({ x = 0, y = 0, radius = 5, color = '0x000000' } = {}) {
+    super();
+    this._x = x;
+    this._y = y;
+    this._radius = radius;
+    this._color = color;
+    /**
+     * @type {PIXI.Graphics}
+     * @private
+     */
+    this._bubble = null;
+
+    this._createBubble();
     this._animateBubbles();
   }
 
-  _init() {}
+  /**
+   * @private
+   */
+  static get events() {
+    return EVENTS;
+  }
+
+  /**
+   * @private
+   */
   _createBubble() {
     const bubble = new Graphics();
-    bubble.beginFill(0x0c59eb);
-    bubble.drawCircle(50, 50, random(2, 7));
+
+    bubble.beginFill(this._color);
+    bubble.drawCircle(this._x, this._y, this._radius);
     bubble.endFill();
     bubble.alpha = 0;
+
     this._bubble = bubble;
     this.addChild(bubble);
   }
-  _generateRandomPoint(xMin, xMax, yMin, yMax) {
-    return {
-      x: this._bubble.x + random(xMin, xMax),
-      y: this._bubble.y + random(yMin, yMax),
-    };
-  }
 
+  /**
+   * Bubble movement animation
+   * @private
+   */
   async _animateBubbles() {
     const tl = new gsap.timeline();
+
     await tl
       .to(this._bubble, {
-        x: 100,
+        x: random(60, 200),
         duration: 5,
         ease: 'Power1.easeIn',
       })
       .to(
         this._bubble,
         {
-          alpha: 1,
-          duration: 2,
+          alpha: 0.1,
+          duration: 1,
         },
-        '<+1'
+        '<1'
       );
+
     await tl
       .to(this._bubble, {
         motionPath: {
           path: [
-            this._generateRandomPoint(-50, 50, -50, -100),
-            this._generateRandomPoint(-50, 50, -100, -150),
-            this._generateRandomPoint(-50, 50, -150, -300),
+            this._generateRandomPoint(40, -40, -50, -100),
+            this._generateRandomPoint(-40, 40, -100, -150),
+            this._generateRandomPoint(-40, 40, -150, -300),
           ],
           align: this.body,
         },
@@ -61,10 +96,21 @@ export default class Bubble extends Container {
           alpha: 0,
           duration: 2,
           onComplete: () => {
-            this.emit('done');
+            this.emit(Bubble.events.ANIMATION_DONE);
           },
         },
         '>-2'
       );
+  }
+
+  /**
+   * Create random point for bubble animation path
+   * @private
+   */
+  _generateRandomPoint(xMin, xMax, yMin, yMax) {
+    return {
+      x: this._bubble.x + random(xMin, xMax),
+      y: this._bubble.y + random(yMin, yMax),
+    };
   }
 }
--- src/components/Progressbar.js
+++ src/components/Progressbar.js
@@ -6,17 +6,49 @@ import gsap from 'gsap';
 import MotionPathPlugin from 'gsap/MotionPathPlugin';
 gsap.registerPlugin(MotionPathPlugin);
 
+/**
+ * Initializes a new instance of Progress
+ * @class
+ * @extends {PIXI.Container}
+ */
 export default class Progress extends Container {
-  constructor() {
+  /**
+   * @param {Number} initialWidth The wave inital width
+   * @param {Number} amplitude The wave amplitude value
+   * @param {Number} bubbleSpawnInterval The bubbles spawn time
+   * @param {Number} bubbleRadiusMin The bubbles radius min value
+   * @param {Number} bubbleRadiusMax The bubbles radius max value
+   */
+  constructor({
+    amplitude = 25,
+    initialWidth = 100,
+    bubbleSpawnInterval = 1000,
+    bubbleRadiusMin = 2,
+    bubbleRadiusMax = 10,
+  } = {}) {
     super();
+    this._amplitude = amplitude;
+    this._initialWidth = initialWidth;
+    this._bubbleSpawnInterval = bubbleSpawnInterval;
+    this._bubbleRadiusMin = bubbleRadiusMin;
+    this._bubbleRadiusMax = bubbleRadiusMax;
 
-    this._path = {
-      size: 200,
-      width: 200,
-      width2: 200,
-    };
-    this._bubbles = [];
-    this.tl = new gsap.timeline();
+    /**
+     * @type {Function}
+     * @private
+     */
+    this._interval = null;
+    /**
+     * @type {PIXI.Graphics}
+     * @private
+     */
+    this._bar = null;
+    /**
+     * @type {Object}
+     * @private
+     */
+    this._path = null;
+
     this._init();
   }
 
@@ -25,21 +57,11 @@ export default class Progress extends Container {
    */
   _init() {
     this._createBar();
+    this._setBarCurvePath();
     this._drawBar();
-    this._animateWaveMovement(30);
+    this._animateWaveMovement();
     this._addEventListener();
-    this._setBubbleSpawnInterval(1000);
-  }
-
-  _addEventListener() {
-    document.addEventListener('click', () => this._increaseProgressBar());
-
-    window.addEventListener('blur', () => {
-      window.clearInterval(this._interval);
-    });
-    window.addEventListener('focus', () => {
-      this._setBubbleSpawnInterval(1000);
-    });
+    this._setBubbleSpawnInterval(this._bubbleSpawnInterval);
   }
 
   /**
@@ -47,30 +69,19 @@ export default class Progress extends Container {
    */
   _createBar() {
     this._bar = new Graphics();
+    this._bar.alpha = 0.1;
     this.addChild(this._bar);
   }
 
   /**
    * @private
    */
-  _setBubbleSpawnInterval(ms) {
-    this._interval = setInterval(() => this._createBubbles(), ms);
-  }
-
-  /**
-   * @private
-   */
-  _createBubbles() {
-    const bubble = new Bubble();
-
-    bubble.x = this._bar.getBounds().right - 90;
-    bubble.y = random(0, window.innerHeight);
-    bubble.on('done', () => {
-      this.removeChild(bubble);
-    });
-
-    this.addChild(bubble);
-    this._bubbles.push(bubble);
+  _setBarCurvePath() {
+    this._path = {
+      width: this._initialWidth,
+      topPartx: this._initialWidth,
+      botPartx: this._initialWidth,
+    };
   }
 
   /**
@@ -79,19 +90,19 @@ export default class Progress extends Container {
   _drawBar() {
     this._bar.clear();
     this._bar.moveTo(0, 0);
-    this._bar.beginFill('0x0C59EB');
-    this._bar.lineTo(this._path.size, 0);
+    this._bar.beginFill('0x000000');
+    this._bar.lineTo(this._path.width, 0);
     this._bar.quadraticCurveTo(
-      this._path.width,
+      this._path.topPartx,
       window.innerHeight / 4,
-      this._path.size,
+      this._path.width,
       window.innerHeight / 2
     );
 
     this._bar.quadraticCurveTo(
-      this._path.width2,
+      this._path.botPartx,
       window.innerHeight - window.innerHeight / 4,
-      this._path.size,
+      this._path.width,
       window.innerHeight
     );
     this._bar.lineTo(-window.innerWidth * 2, window.innerHeight);
@@ -104,17 +115,16 @@ export default class Progress extends Container {
    * @param {Number} amplitude wave amplitude
    * @private
    */
-  async _animateWaveMovement(amplitude) {
-    this._path.width = this._path.size;
+  async _animateWaveMovement() {
     gsap.fromTo(
       this._path,
       {
-        width: this._path.size - amplitude,
-        width2: this._path.size + amplitude,
+        topPartx: this._path.width - this._amplitude,
+        botPartx: this._path.width + this._amplitude,
       },
       {
-        width: this._path.size + amplitude,
-        width2: this._path.size - amplitude,
+        topPartx: this._path.width + this._amplitude,
+        botPartx: this._path.width - this._amplitude,
         yoyo: true,
         repeat: -1,
         duration: 3,
@@ -130,10 +140,37 @@ export default class Progress extends Container {
   /**
    * @private
    */
-  _increaseProgressBar() {
-    gsap.to(this._bar, {
-      x: '+=50',
-      duration: 3,
+  _addEventListener() {
+    window.addEventListener('blur', () => {
+      window.clearInterval(this._interval);
     });
+    window.addEventListener('focus', () => {
+      this._setBubbleSpawnInterval(this._bubbleSpawnInterval);
+    });
+  }
+
+  /**
+   * @param {Number} ms interval milliseconds
+   * @private
+   */
+  _setBubbleSpawnInterval(ms) {
+    this._interval = setInterval(() => this._createBubble(), ms);
+  }
+
+  /**
+   * @private
+   */
+  _createBubble() {
+    const bubble = new Bubble({
+      radius: random(this._bubbleRadiusMin, this._bubbleRadiusMax),
+    });
+
+    bubble.x = this._bar.getBounds().right;
+    bubble.y = random(0, window.innerHeight);
+    bubble.on(Bubble.events.ANIMATION_DONE, () => {
+      this.removeChild(bubble);
+    });
+
+    this.addChild(bubble);
   }
 }
