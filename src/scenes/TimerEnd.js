203f6a3cea18196191f67106a452ab1a18a6fdef
Simona <monisimi99@gmail.com>
2021-04-10 01:37:16 +0300

    feat: added timer end scene

--- src/scenes/TimerEnd.js
+++ src/scenes/TimerEnd.js
@@ -1,27 +1,46 @@
-import Scene from "./Scene";
-import { Sprite } from "pixi.js";
-import Title from "../components/Title";
-import Button from "../components/Button";
-import Background from "../components/Background";
-import FireworkParticle from "../components/FireworkParticle";
+import Scene from './Scene';
+import { Text } from 'pixi.js';
+import Title from '../components/Title';
+import Background from '../components/Background';
+import Firework from '../components/Firework';
 
-export default class Intro extends Scene {
+export default class TimerEnd extends Scene {
   constructor() {
     super();
   }
 
   onCreated() {
-    this.drawBackground();
-    this.drawParticle();
+    this._drawBackground();
+    this._drawFirework();
+    this._addText();
+    this._drawTitle();
   }
 
-  drawBackground() {
+  _drawBackground() {
     const background = new Background();
     this.addChild(background);
   }
 
-  drawParticle() {
-    const particle = new FireworkParticle();
-    this.addChild(particle);
+  _drawFirework() {
+    const firework = new Firework();
+    this.addChild(firework);
+  }
+
+  _addText() {
+    const text = new Text('Time\'s UP!', {
+      fill: ['#FFFFFF', '#000000'],
+      fillGradientType: 1,
+      fontFamily: 'Raleway',
+      fontSize: 200,
+      fontWeight: 600,
+    });
+    text.anchor.set(0.5);
+    this.addChild(text);
+  }
+
+  _drawTitle() {
+    const title = new Title('Please wait while the tasks are rated');
+    title.y = 200;
+    this.addChild(title);
   }
 }
