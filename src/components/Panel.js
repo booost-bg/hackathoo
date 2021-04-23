80fb3ec045a057a61028fb3b2d5060a19e3d7211
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-04-23 12:26:08 +0300

    refactor: rules and criteria are created in countounBase

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,4 +1,4 @@
-import gsap from "gsap/all";
+import gsap from 'gsap/all';
 
 /**
  * Represents the rules/criteria panel.
@@ -14,10 +14,45 @@ export default class Panel {
     this._title = title;
     this._content = content;
     this._tl = gsap.timeline();
+  }
+
+  /**
+   * Initializes the component.
+   * @method
+   * @public
+   */
+  init() {
     this._generateTitleElement();
     this._generateTextArea();
     this._generateExitButton();
     this._generateDomElement();
+    this._animateIn();
+  }
+
+  /**
+   * Animates the dom element in.
+   * @method
+   * @private
+   */
+  _animateIn() {
+    this._tl.from(this.domElement, {
+      x: 1000,
+    });
+  }
+
+  /**
+   * Animates the dom element out.
+   * @method
+   * @private
+   */
+  _animateOut() {
+    this._tl
+      .to(this.domElement, {
+        x: 1000,
+      })
+      .to(this.domElement, {
+        display: 'none',
+      });
   }
 
   /**
@@ -26,21 +61,21 @@ export default class Panel {
    * @private
    */
   _generateTitleElement() {
-    this._titleElement = document.createElement("h1");
+    this._titleElement = document.createElement('h1');
 
     Object.assign(this._titleElement.style, {
-      display: "flex",
-      "flex-direction": "column",
-      "font-family": "Raleway",
-      "font-size": "60px",
-      "font-weight": "800",
-      position: "absolute",
-      "justify-content": "space-between",
-      "box-sizing": "border-box",
-      "align-items": "flex-start",
-      top: "50px",
+      display: 'flex',
+      'flex-direction': 'column',
+      'font-family': 'Raleway',
+      'font-size': '60px',
+      'font-weight': '800',
+      position: 'absolute',
+      'justify-content': 'space-between',
+      'box-sizing': 'border-box',
+      'align-items': 'flex-start',
+      top: '50px',
       left: `1150px`,
-      "z-index": "1",
+      'z-index': '1',
     });
 
     this._titleElement.innerText = this._title;
@@ -52,24 +87,24 @@ export default class Panel {
    * @private
    */
   _generateTextArea() {
-    this._textArea = document.createElement("textarea");
+    this._textArea = document.createElement('textarea');
     this._textArea.readOnly = true;
     Object.assign(this._textArea.style, {
-      outline: "none",
-      display: "flex",
-      "flex-direction": "column",
-      "font-family": "Raleway",
-      "font-size": "18px",
-      padding: "68px",
-      "padding-top": "180px",
+      outline: 'none',
+      display: 'flex',
+      'flex-direction': 'column',
+      'font-family': 'Raleway',
+      'font-size': '18px',
+      padding: '68px',
+      'padding-top': '180px',
       height: `${window.innerHeight}px`,
       width: `${window.innerWidth / 2.3}px`,
-      position: "absolute",
-      "justify-content": "space-between",
-      "box-sizing": "border-box",
-      "align-items": "flex-start",
+      position: 'absolute',
+      'justify-content': 'space-between',
+      'box-sizing': 'border-box',
+      'align-items': 'flex-start',
       left: `1085px`,
-      "z-index": "0",
+      'z-index': '0',
     });
 
     this._textArea.innerText = this._content;
@@ -81,42 +116,34 @@ export default class Panel {
    * @private
    */
   _generateExitButton() {
-    this._exitButton = document.createElement("button");
+    this._exitButton = document.createElement('button');
 
     Object.assign(this._exitButton.style, {
-      outline: "none",
-      border: "none",
-      display: "flex",
-      "flex-direction": "column",
-      "font-family": "Raleway",
-      "font-size": "50px",
-      "font-weight": "800",
+      outline: 'none',
+      border: 'none',
+      display: 'flex',
+      'flex-direction': 'column',
+      'font-family': 'Raleway',
+      'font-size': '50px',
+      'font-weight': '800',
       height: `70px`,
       width: `70px`,
-      color: "white",
-      "background-color": "black",
-      position: "absolute",
-      "justify-content": "center",
-      "box-sizing": "border-box",
-      "align-items": "center",
+      color: 'white',
+      'background-color': 'black',
+      position: 'absolute',
+      'justify-content': 'center',
+      'box-sizing': 'border-box',
+      'align-items': 'center',
       left: `${window.innerWidth - 70}px`,
-      cursor: "pointer",
-      "z-index": "1",
+      cursor: 'pointer',
+      'z-index': '1',
     });
 
-    this._exitButton.innerHTML = "x";
+    this._exitButton.innerHTML = 'x';
 
     this._exitButton.addEventListener(
-      "click",
-      () => {
-        this._tl
-          .to(this.domElement, {
-            x: 1000,
-          })
-          .to(this.domElement, {
-            display: "none",
-          });
-      },
+      'click',
+      () => this._animateOut(),
       {
         once: true,
       }
@@ -129,17 +156,17 @@ export default class Panel {
    * @private
    */
   _generateDomElement() {
-    this.domElement = document.createElement("div");
+    this.domElement = document.createElement('div');
     Object.assign(this.domElement.style, {
-      "font-family": "Raleway",
-      width: "100%",
-      height: "100%",
-      margin: "0",
-      padding: "0",
-      overflow: "hidden",
-      display: "flex",
-      position: "absolute",
-      "flex-direction": "column",
+      'font-family': 'Raleway',
+      width: '100%',
+      height: '100%',
+      margin: '0',
+      padding: '0',
+      overflow: 'hidden',
+      display: 'flex',
+      position: 'absolute',
+      'flex-direction': 'column',
     });
     this.domElement.appendChild(this._textArea);
     this.domElement.appendChild(this._titleElement);
